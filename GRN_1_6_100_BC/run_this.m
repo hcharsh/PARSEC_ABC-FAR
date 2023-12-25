4bfb14c91ee9515ad65456faefa0d98f89b82215
hcharsh <71984307+hcharsh@users.noreply.github.com>
2023-12-25 11:24:31 +0530

    Add files via upload

---
+++ GRN_1_6_100_BC/run_this.m
@@ -0,0 +1,19 @@
+clear all; close all; clc; warning off;
+
+%% TSA analysis
+run('TSA\Model_efast.m');
+run('TSA\plot_TSA.m');
+% % run('TSA/plot_TSA_sep'); % with NaN rows separating the variables
+
+%% Clustering and experiment design
+run('PARSEC\make_designs.m');
+run('PARSEC\gen_data_GRN.m');
+
+%% model fitting and evaluation of the designs
+run('expt_analysis\macro_GRN.m');
+run('expt_analysis\analysis_script_fin.m');
+
+% % post-evaluation
+% run('expt_analysis\visualize_eval.m');
+% % % mention the clustering rlz number as expt_ind, in the following file
+% run('clustering/overlap_dyn_clustering.m');
